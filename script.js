549b98c54d3a1c7da299f755406f9720b9203dda
Luis Ramirez <sdaewa22@gmail.com>
2020-10-09 13:15:15 +0100

    API call

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
  async function getQuote() {
      const API = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
      try {
-         const reponse = await fetch(API);
-         const data = await Response.json();
-         console.loog(data);
+         const response = await fetch(API);
+         const data = await response.json();
+         console.log(data);
      } catch (error) {
          console.log('Whoops no quote!', error);
      }
