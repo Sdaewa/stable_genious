efb905e03f25115423f5fd7fef5c68da6d9faf97
Luis Ramirez <sdaewa22@gmail.com>
2020-10-12 12:34:40 +0100

    API

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
- async function getQuote() {
-     const API = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-     try {
-         const response = await fetch(API);
-         const data = await response.json();
-         console.log(data);
-     } catch (error) {
-         console.log('Whoops no quote!', error);
-     }
- }
+const quoteContainer = document.getElementById('quote-container');
+const messageText = document.getElementById('message');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
- getQuote();
+
+
+async function getQuote() {
+    const API = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+    try {
+        const response = await fetch(API);
+        const data = await response.json();
+        console.log(data)
+        messageText.innerHTML = data.message;
+        // authorText.innerText = data.quoteText;
+
+    } catch (error) {
+        alert(error)
+    }
+}
+getQuote();
