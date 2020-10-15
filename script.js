d3c54776bdf8e4a9b437df4d1fdf6a942d219e2a
Luis Ramirez <sdaewa22@gmail.com>
2020-10-15 16:28:39 +0100

    validating with REGEX

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ async function getQuote() {
         const response = await fetch(API);
         const data = await response.json();
         messageText.innerText = data.value;
+        if (messageText.innerText === data.value)
+            console.log(data.value)
         date.innerText = new Date(data.appeared_at).toDateString();
         source = data._embedded.source[0].url;
         if (data.value.length > 120) {
@@ -30,6 +32,15 @@ async function getQuote() {
     } catch (error) {
         getQuote();
     };
+}
+
+function is_url(str) {
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function loadingSpinner() {
