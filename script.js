7907b528153e0ae744983b24ad9f971f462c9a34
Luis Ramirez <sdaewa22@gmail.com>
2020-10-16 12:42:07 +0100

    x

--- script.js
+++ script.js
@@ -18,8 +18,12 @@ async function getQuote() {
         const response = await fetch(API);
         const data = await response.json();
         messageText.innerText = data.value;
-        if (messageText.innerText === data.value)
-            console.log(data.value)
+        if (is_url(data.value)) {
+            return console.log(messageText.innerText = data.value, hyperLink);
+        };
+        if (!is_url(data.value)) {
+            return console.log(messageText.innerText = data.value);
+        }
         date.innerText = new Date(data.appeared_at).toDateString();
         source = data._embedded.source[0].url;
         if (data.value.length > 120) {
@@ -29,17 +33,17 @@ async function getQuote() {
         }
 
         completeSpinner();
-    } catch (error) {
-        getQuote();
-    };
-}
 
-function is_url(str) {
-    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-    if (regexp.test(str)) {
-        return true;
-    } else {
-        return false;
+    } catch (error) {
+        // getQuote();
+        alert(error)
+    };
+
+
+    function is_url(str) {
+        regexp = /([\w+]+\:\/\/)?()*[\w-]+[\.\:]\w+([\/\?\]?[\w-]+)*\/?/igm;
+        let hyperLink = str.match(regexp);
+        return hyperLink;
     }
 }
 
